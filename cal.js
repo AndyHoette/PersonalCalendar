e96dd18aa958dba27d6a9a98d050a208a64b44ff
Andy-east <a.h.hoette@wustl.edu>
2024-10-28 11:33:53 -0500

    logging answer

--- cal.js
+++ cal.js
@@ -213,6 +213,7 @@ function logIn(){
         })
             .then(response => response.json())
             .then(answer => { //returns a csrfToken, userID, and success bool
+                console.log(answer);
                 if (answer.success) {
                     console.log("log in good");
                     document.getElementById("LogInForm").style.display = "none";
