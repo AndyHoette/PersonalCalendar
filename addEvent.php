04be0efcc4d2dfcf5bf0188c1a0ab6c41858b61a
Joshua Lee <j.m.lee@wustl.edu>
2024-10-28 12:25:54 -0500

    addEvent

--- addEvent.php
+++ addEvent.php
@@ -7,7 +7,7 @@ header("Content-Type: application/json");
 $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str, true);
 // Check if user is logged in
-if (empty($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) {
     echo json_encode(array(
         "success" => false,
         "message" => "User not logged in."
