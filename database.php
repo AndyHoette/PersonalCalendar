b3c38a477875dde8de9f206421860a9cb9008a09
Joshua Lee <j.m.lee@wustl.edu>
2024-10-27 22:03:39 -0500

    editdatabase

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'viewer', 'easypassword', 'leeHoetteCal');
+$mysqli = new mysqli('localhost', 'test', 'password', 'leeHoetteCal');
 
 if ($mysqli->connect_errno) {
     printf("Connection Failed: %s\n", $mysqli->connect_error);
