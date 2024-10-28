6526a60393e28c101729b5684ebda01a748c817f
Andy-east <a.h.hoette@wustl.edu>
2024-10-28 08:28:32 -0500

    added fields not set to login

--- login_ajax.php
+++ login_ajax.php
@@ -13,6 +13,13 @@ $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str, true);
 
 
+if(!isset($json_obj['userID']) || !isset($json_obj['password'])){
+    echo json_encode(array(
+        "success" => false,
+        "message" => "fields not set"
+    ));
+}
+
 $user_id = $json_obj['userID'];
 $passwordGuess = $json_obj['password'];
 
